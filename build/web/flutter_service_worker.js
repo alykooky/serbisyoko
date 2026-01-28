6cf76a4084b89e137639d2232d43065338bfe512
kylaapostol1@gmail.com <kylaapostol1@gmail.com>
2026-01-28 12:10:28 +0800

    Update Flutter web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -44,7 +44,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0c3bfcf94c649c64dda7c3880e17c69c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "e0745fa019f4a14b6f6d95be9ffb8a3e",
+"flutter_bootstrap.js": "a770be9f1f33ad3f454934050f94b354",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
